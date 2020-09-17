736932d028513c515634c803b3e24908d723ae4b
aurilisdev <aurilisdev@gmail.com>
2020-09-17 20:32:53 +0200

    Update to 1.16.3

--- src/main/java/blastcraft/References.java
+++ src/main/java/blastcraft/References.java
@@ -5,6 +5,6 @@ import blastcraft.common.tab.ItemGroupBlastcraft;
 public class References {
 	public static final String ID = "blastcraft";
 	public static final String NAME = "Blastcraft";
-	public static final String VERSION = "0.0.2-1.16.1";
+	public static final String VERSION = "0.0.2-1.16.3";
 	public static final ItemGroupBlastcraft CORETAB = new ItemGroupBlastcraft("itemgroupblastcraft");
 }
