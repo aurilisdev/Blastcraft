84a14a0cc18326941890a91d408d3b39dfd379cd
aurilisdev <aurilisdev@gmail.com>
2021-03-17 17:08:36 +0100

    Add particles

--- src/main/java/blastcraft/common/tile/TileBlastCompressor.java
+++ src/main/java/blastcraft/common/tile/TileBlastCompressor.java
@@ -16,6 +16,7 @@ import electrodynamics.api.tile.components.type.ComponentTickable;
 import electrodynamics.common.inventory.container.ContainerO2OProcessor;
 import electrodynamics.common.item.ItemProcessorUpgrade;
 import electrodynamics.common.recipe.MachineRecipes;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Direction;
 
 public class TileBlastCompressor extends GenericTileTicking {
@@ -26,7 +27,7 @@ public class TileBlastCompressor extends GenericTileTicking {
 		.addRelativeSlotsOnFace(Direction.EAST, 1).addRelativeSlotsOnFace(Direction.WEST, 2)
 		.setItemValidPredicate((slot, stack) -> slot == 0 || slot > 2 && stack.getItem() instanceof ItemProcessorUpgrade));
 	addComponent(new ComponentPacketHandler());
-	addComponent(new ComponentTickable());
+	addComponent(new ComponentTickable().addTickClient(this::tickClient));
 	addComponent(new ComponentElectrodynamic(this).setVoltage(CapabilityElectrodynamic.DEFAULT_VOLTAGE * 2)
 		.addRelativeInputDirection(Direction.NORTH));
 	addComponent(new ComponentProcessor(this).addUpgradeSlots(2, 3, 4).setJoulesPerTick(Constants.BLASTCOMPRESSOR_USAGE_PER_TICK)
@@ -34,5 +35,16 @@ public class TileBlastCompressor extends GenericTileTicking {
 		.setProcess(component -> MachineRecipes.process(this)).setType(ComponentProcessorType.ObjectToObject));
 	addComponent(new ComponentContainerProvider("container.blastcompressor").setCreateMenuFunction(
 		(id, player) -> new ContainerO2OProcessor(id, player, getComponent(ComponentType.Inventory), getCoordsArray())));
+    }
+
+    protected void tickClient(ComponentTickable tickable) {
+	if (this.<ComponentProcessor>getComponent(ComponentType.Processor).operatingTicks > 0 && world.rand.nextDouble() < 0.15) {
+	    Direction direction = this.<ComponentDirection>getComponent(ComponentType.Direction).getDirection();
+	    double d4 = world.rand.nextDouble();
+	    double d5 = direction.getAxis() == Direction.Axis.X ? direction.getXOffset() * (direction.getXOffset() == -1 ? 0 : 1) : d4;
+	    double d6 = world.rand.nextDouble();
+	    double d7 = direction.getAxis() == Direction.Axis.Z ? direction.getZOffset() * (direction.getZOffset() == -1 ? 0 : 1) : d4;
+	    world.addParticle(ParticleTypes.SMOKE, pos.getX() + d5, pos.getY() + d6, pos.getZ() + d7, 0.0D, 0.0D, 0.0D);
+	}
     }
 }
