cb358bc020dcac92eccea79bd793fd460631bcae
skip999 <skiprocks999@gmail.com>
2021-06-25 13:26:59 -0500

    Clean up JEI and move CustomGlass to subtype
    Made CustomGlassBlock a subtype in Electrodynamics so now that other mods can add blocks (like ALON). Also cleaned up the JEI plugin a bit.

--- src/main/java/blastcraft/Blastcraft.java
+++ src/main/java/blastcraft/Blastcraft.java
@@ -1,10 +1,10 @@
 package blastcraft;
 
 import blastcraft.client.render.tile.RenderCamoflage;
-import blastcraft.common.block.BlockCustomBrickGlass;
-import blastcraft.common.settings.Constants;
-import electrodynamics.prefab.configuration.ConfigurationHandler;
 import blastcraft.common.recipe.BlastCraftRecipeInit;
+import blastcraft.common.settings.Constants;
+import electrodynamics.common.block.BlockCustomGlass;
+import electrodynamics.prefab.configuration.ConfigurationHandler;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
@@ -39,7 +39,7 @@ public class Blastcraft {
     @OnlyIn(Dist.CLIENT)
     public static void onClientSetup(FMLClientSetupEvent event) {
 	for (RegistryObject<Block> block : DeferredRegisters.BLOCKS.getEntries()) {
-	    if (block.get() instanceof BlockCustomBrickGlass) {
+	    if (block.get() instanceof BlockCustomGlass) {
 		RenderTypeLookup.setRenderLayer(block.get(), RenderType.getCutout());
 	    }
 	}
