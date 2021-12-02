2af53f36701877602e4272f151962a6a387d548d
skip999 <skiprocks999@gmail.com>
2021-12-01 21:13:33 -0600

    updated recipes to new format
    > updated recipes to new format
    > updated JEI to new format

--- src/main/java/blastcraft/common/recipe/BlastCraftRecipeInit.java
+++ src/main/java/blastcraft/common/recipe/BlastCraftRecipeInit.java
@@ -1,7 +1,7 @@
 package blastcraft.common.recipe;
 
-import blastcraft.common.recipe.categories.o2o.O2ORecipeTypes;
-import blastcraft.common.recipe.categories.o2o.specificmachines.BlastCompressorRecipe;
+import blastcraft.common.recipe.categories.item2item.Item2ItemRecipeTypes;
+import blastcraft.common.recipe.categories.item2item.specificmachines.BlastCompressorRecipe;
 import electrodynamics.common.recipe.ElectrodynamicsRecipeInit;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -16,7 +16,7 @@ public class BlastCraftRecipeInit {
 
     /* RECIPE TYPES */
 
-    // O2O
+    // ITEM2ITEM
     public static final RecipeType<BlastCompressorRecipe> BLAST_COMPRESSOR_TYPE = ElectrodynamicsRecipeInit
 	    .registerType(BlastCompressorRecipe.RECIPE_ID);
 
@@ -24,5 +24,5 @@ public class BlastCraftRecipeInit {
 
     // O2O
     public static final RegistryObject<RecipeSerializer<?>> BLAST_COMPRESSOR_SERIALIZER = RECIPE_SERIALIZER
-	    .register(BlastCompressorRecipe.RECIPE_GROUP, () -> O2ORecipeTypes.BLAST_COMPRESSOR_JSON_SERIALIZER);
+	    .register(BlastCompressorRecipe.RECIPE_GROUP, () -> Item2ItemRecipeTypes.BLAST_COMPRESSOR_JSON_SERIALIZER);
 }
