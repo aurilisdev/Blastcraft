f41204a2175ccfdb471fe2a4f35c82352e4a3932
aurilisdev <aurilisdev@gmail.com>
2026-06-13 22:30:38 +0200

    Patch 2: Fix #66 and #64, Merge #65

--- src/main/java/blastcraft/common/block/BlockCustomWall.java
+++ src/main/java/blastcraft/common/block/BlockCustomWall.java
@@ -5,7 +5,7 @@ import net.minecraft.world.level.block.WallBlock;
 
 public class BlockCustomWall extends WallBlock {
 
-	public BlockCustomWall(float resistance, float hardness) {
+	public BlockCustomWall(float hardness, float resistance) {
 		super(Properties.copy(Blocks.IRON_BLOCK).requiresCorrectToolForDrops().strength(hardness, resistance));
 	}
 
